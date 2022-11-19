10de7cb20e493b448685839437001d1e8b5d2b02
¨Aladino <aladino@headhunters.digital>
2022-11-19 20:53:41 -0300

    log[19.11.022]: python exercise solution
    - resistor color list logic with unitary tests and readme

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def color_code(color: str) -> int:
+    return COLORS.index(color.lower())
+
+
+def colors() -> list[str]:
+    return COLORS
